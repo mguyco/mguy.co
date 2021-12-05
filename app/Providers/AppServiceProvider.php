2f3641908317c3982e17278a97cfc69140e3a3a0
mguy <mguy.co@gmail.com>
2021-12-04 19:14:52 -0500

    heroku updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(env('APP_ENV') == 'production') {
+            \URL::forceScheme('https');
+        }
+
         Inertia::share([
             'errors' => function () {
                 return Session::get('errors')
